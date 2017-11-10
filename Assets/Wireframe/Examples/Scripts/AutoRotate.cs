f056441568a5379a93a2498f0f893d4d53c0fd91
Chase Pettit <chasepettit@gmail.com>
2017-11-10 13:25:36 -0500

    Add examples.

---
+++ Assets/Wireframe/Examples/Scripts/AutoRotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace SuperSystems.UnityTools
+{
+
+public class AutoRotate : MonoBehaviour
+{
+    public Vector3 speed = new Vector3(10, 10, 10);
+    public Space space = Space.World;
+
+    void Update()
+    {
+        transform.Rotate(speed * Time.deltaTime, space);
+    }
+}
+
+}
